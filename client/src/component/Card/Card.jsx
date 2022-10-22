de9ea5b4eb9977a7e489be339486af89f531b7ff
JSebastian101 <jhonsp101@gmail.com>
2022-10-22 18:15:23 -0500

    borre el sub contenedor de las cards, entonces todas pasan a ser hijas de .container y les doy un display flex con flex-wrap para que bajen de forma automatica. le doy a cada tarjeta el 40%. en una media query cambio el display-direction:row a column

--- client/src/component/Card/Card.jsx
+++ client/src/component/Card/Card.jsx
@@ -3,14 +3,10 @@ import s from './Card.module.css';
 function Card() {
   return (
     <main className={s.container}>
-      <div className={s.orden}>
-        <div className={s.card1}></div>
         <div className={s.card}></div>
-      </div>
-      <div className={s.orden}>
-        <div className={s.card1}></div>
         <div className={s.card}></div>
-      </div>
+        <div className={s.card}></div>
+        <div className={s.card}></div>
     </main>
   );
 }
